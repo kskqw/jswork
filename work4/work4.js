408ccd10b3c2e8fcd89ad154eec1c64cb952e095
kskqw <2463263496@qq.com>
2019-10-17 11:59:01 +0800

    work5

---
+++ work4/work4.js
@@ -0,0 +1,5 @@
+document.write("<p>金字塔</p>")
+let level = prompt('请设置金字塔的层数')
+if (isNaN(level)){
+    alert('金字塔的层数是数字')
+}
---
+++ work5/work5.js
@@ -0,0 +1,33 @@
+//9x9的空表
+str ='<table border="1">'
+for(let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=9;j>0;--j){
+        str +='<td>&nbsp</td>'
+    }
+    str +='</tr>'
+}
+    str +='</table>'
+    document.getElementById('table2').innerHTML = str
+//倒九九乘法空表
+str ='<table border="1">'
+for(let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>&nbsp</td>'
+    }
+    str +='</tr>'
+}
+    str +='</table>'
+    document.getElementById('table2').innerHTML = str
+     //倒九九乘法表
+    str ='<table border="1">'
+for(let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>+j+'*'+(j*i)+</td>';
+    }
+    str +='</tr>'
+}
+    str +='</table>'
+    document.getElementById('table2').innerHTML = str
