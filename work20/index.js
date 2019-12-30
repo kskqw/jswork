8d7258e64ce7770b1b3c60c36feea0eaaebebb22
kskqw <2463263496@qq.com>
2019-12-30 21:28:22 +0800

    jswork

--- work20/index.js
+++ work20/index.js
@@ -1,12 +1,11 @@
-$('#fold>ul>li:first').find('.wrap').css({
+$('#fold>ul>li>:first').find('.wrap').css({
     display: 'block'
 });
-
-$('#flod>ul>li').click(function () {
+$('#fold>ul>li').click(function () {
     $(this).siblings('li').find('.wrap').css({
         display: 'none'
     });
     $(this).find('.wrap').css({
-         display: 'block'
+        display: 'block'
     });
 });
